5449cd3e89c8a9872f6466580efac57fa488ad32
yadalla sreenija <sreeyadalla@gmail.com>
2025-02-24 10:54:48 +0530

    done

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express =require('express')
 require('dotenv').config()
 const cors = require('cors')
-const port=process.env.PORT
+const port=process.env.PORT || 2222
+const mongoose = require('mongoose')
+const app =express()
+
+const db = async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI),
+        console.log("Connected to mongodb");
+    }
+    catch(e){
+        console.error("Failed to connect mongodb")
+    }
+}
+db()
 
 app.use(cors())
 app.use(express.json())
